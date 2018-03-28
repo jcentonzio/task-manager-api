58795424b33ff94112a1b1db497e8ce6f225aa58
Jorge <jcentonzio@gmail.com>
2018-03-27 23:01:09 -0300

    changing double quotes to simple quotes

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@ FactoryGirl.define do
 
   factory :user do 
     #email {Faker::Internet.email}
-    email "jcentonzio@gmail.com"
-    password "123456"
-    password_confirmation "123456"
+    email 'jcentonzio@gmail.com'
+    password '123456'
+    password_confirmation '123456'
   end  
 
 end
