fc33b9ced439e41bb4fea213781091fd0bd3eab3
Jorge <jcentonzio@gmail.com>
2018-03-28 00:26:17 -0300

    adding create action to the users controller with test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do 
 
   factory :user do 
-    #email {Faker::Internet.email}
-    email 'jcentonzio@gmail.com'
+    email {Faker::Internet.email}
     password '123456'
     password_confirmation '123456'
   end  
