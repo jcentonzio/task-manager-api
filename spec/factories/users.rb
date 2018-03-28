e07049c2dd5174762291c07d1f0d9014240e5b24
Jorge <jcentonzio@gmail.com>
2018-03-27 22:21:17 -0300

    adding show action to ther user controller

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,8 @@
 FactoryGirl.define do 
 
   factory :user do 
-    email {Faker::Internet.email}
+    #email {Faker::Internet.email}
+    email "jcentonzio@gmail.com"
     password "123456"
     password_confirmation "123456"
   end  
