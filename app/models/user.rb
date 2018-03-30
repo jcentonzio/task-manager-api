08f5809e57f7802e66b361b6dd3b92e602d7d4b1
Jorge <jcentonzio@gmail.com>
2018-03-30 11:59:37 -0300

    adding auth token to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   #validates_presence_of :name
   validates_presence_of :email
 
+  validates_uniqueness_of :auth_token
+
+
+  def info
+    "#{email} - #{created_at}"
+  end
          
 
 end
