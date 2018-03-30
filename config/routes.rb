821db255ee64a76b3fdd0f8e5b18f2c4364d4524
Jorge <jcentonzio@gmail.com>
2018-03-30 15:51:12 -0300

    Addig create action to the session controller with test

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ require 'api_version_constraint'
 
 Rails.application.routes.draw do
   
-  #devise_for :users
+  devise_for :users, only: [:sessions], controllers: { sessions: 'api/v1/sessions' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do  
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true)  do 
      resources :tasks
      resources :users, only: [:show, :create, :update, :destroy]
+     resources :sessions, only: [:create] 
    end
 
   end	
