0bcb85e48e007660920c13c375986c465156b133
Jorge <jcentonzio@gmail.com>
2018-04-01 20:40:19 -0300

    adding update action to the tasks controller with tests

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -92,6 +92,50 @@ RSpec.describe 'Task API' do
     
       
   end
+
+
+  describe 'PUT /tasks/:id' do 
+    let!(:task) { create(:task, user_id: user.id) }
+    before do 
+      put "/tasks/#{task.id}", params: {task: task_params }.to_json, headers: headers    
+    end    
+
+    context 'when the params are valid' do 
+      let(:task_params) { { title: 'New task title' } }
+      
+      it 'return status code 200' do 
+        expect(response).to have_http_status(200)  
+      end
+      
+      it 'return the json for update test' do 
+        expect(json_body[:title]).to eq(task_params[:title]) 
+      end  
+
+      it 'updates the task in the database' do 
+        expect( Task.find_by(title: task_params[:title]) ).not_to be_nil
+      end  
+
+    end  
+    
+    context 'when the params are invalid' do 
+      let(:task_params) { { title: ' '} }
+
+      it 'return status code 422' do 
+        expect(response).to have_http_status(422)  
+      end
+
+      it 'return the json error for title' do 
+        expect(json_body[:errors]).to have_key(:title)
+      end  
+
+      it 'does not update the task in the database' do 
+        expect( Task.find_by(title: task_params[:title]) ).to be_nil
+      end  
+
+    end    
+
+
+  end  
     
 
 
