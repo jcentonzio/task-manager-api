82b9d54c1c9544c37e211fab24f300d07b969469
Jorge <jcentonzio@gmail.com>
2018-04-09 23:13:06 -0300

    adjusting controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 require "application_responder"
 
 class ApplicationController < ActionController::API
-  include DeviseTokenAuth::Concerns::SetUserByToken
-  include Authenticable   
 
 end
--- spec/controllers/application_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ApplicationController, type: :controller do
-  describe 'includes the correc concerns' do
-    it { expect(controller.class.ancestors).to include(Authenticable) }
-  end
-          
-end
-    
